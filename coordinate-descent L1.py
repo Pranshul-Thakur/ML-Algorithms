0603042243bbd9f8e988264ba0a82f4f3a5556cc
Pranshul-Thakur <pranshulthakur.11@gmail.com>
2025-03-17 18:23:07 +0530

    removals

--- coordinate-descent L1.py
+++ coordinate-descent L1.py
@@ -4,8 +4,6 @@ x = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
 y = np.array([3, 5, 7, 9])
 
 penalty = 0.1
-columns = x.shape[1]
-I = np.eye(columns)
 
 B = np.linalg.inv(x.T @ x) @ x.T @ y
 w = np.sign(B) * (np.abs(B) - penalty)
