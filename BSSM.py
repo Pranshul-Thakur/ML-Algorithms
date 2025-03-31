6e44d92caaa74513ce1598558f9b81225f82ae33
Pranshul-Thakur <pranshulthakur.11@gmail.com>
2025-03-31 23:21:55 +0530

    round

--- BSSM.py
+++ BSSM.py
@@ -3,7 +3,8 @@ import math
 def softmax(array):
     expi = [math.exp(i) for i in array]
     sum_expi = sum(expi)
-    return [i / sum_expi for i in expi]
+    # return [i / sum_expi for i in expi] normal method
+    return [round(i / sum_expi, 3) for i in expi]
 
 array = [1, 2, 3]
 
